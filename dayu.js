0ed07e79b1a21b0da4fb1d9dc135864729a2c3b0
lychee <lychee930224@gmail.com>
2023-04-05 17:47:56 +0800

    modify

--- dayu.js
+++ dayu.js
@@ -4,6 +4,7 @@ const Mock = require('mockjs')
 const app = express()
 
 app.get("/api/users", (req, res) => {
+
   const userList = Mock.mock({
     'userList|10': [{
       'id|+1': 1,
@@ -11,10 +12,6 @@ app.get("/api/users", (req, res) => {
       'email': '@email'
     }]
   })
-
-  setTimeout(() => {
-    throw new Error('服务器故障')
-  }, 5000)
 
   res.status(200)
   res.json(userList)
