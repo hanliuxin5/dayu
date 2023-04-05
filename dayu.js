e2c1d3852a97632442833c27feefaed6d40dca9b
lychee <lychee930224@gmail.com>
2023-04-05 20:10:07 +0800

    for test

--- dayu.js
+++ dayu.js
@@ -2,15 +2,24 @@ const express = require('express')
 
 var app = express();
 
+app.use('/public', express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.sendFile(__dirname + "/" + "index.html");
+})
+
+app.get('/process_get', function (req, res) {
+
+  var response = {
+    "question": req.query.first_name,
+  };
+
+  res.end(JSON.stringify(response));
 })
 
 var server = app.listen(3000, function () {
 
   var host = server.address().address
   var port = server.address().port
-
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
 
 })
