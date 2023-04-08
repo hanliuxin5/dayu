1e10c629a6669435cd41b35d177ef9628f5517da
lychee <lychee930224@gmail.com>
2023-04-08 20:56:27 +0800

    for test

--- dayu.js
+++ dayu.js
@@ -3,7 +3,7 @@ const ai = require('openai')
 
 const configuration = new ai.Configuration({
   organization: "org-0pFCFj24UOWqNTSz1GNyKxsG",
-  apiKey: "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",
+  apiKey: process.env.OPEN_KEY,
 });
 const openai = new ai.OpenAIApi(configuration);
 
@@ -13,28 +13,28 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + "/" + "index.html");
 })
 
-var messages = new Array();
-messages.push({ role: "system", content: "You are a helpful assistant" })
+// var messages = new Array();
+// messages.push({ role: "system", content: "You are a helpful assistant" })
 
 app.get('/process_get', runAsyncWrapper(async (req, res) => {
 
   const tmpMessages = [
     // { role: "system", content: "You are a helpful assistant" },
-    // { role: "user", content: "hello" },
+    { role: "user", content: "hello" },
     // { role: "assistant", content: "Hello! How can I assist you today?" },
     // { role: "user", content: "what i just say?" },
     // { role: "assistant", content: "You just said 'hello'. Is there anything else you would like to say or ask?" },
     // { role: "user", content: req.query.question },
   ];
-  messages.push({ role: "user", content: req.query.question })
+  // messages.push({ role: "user", content: req.query.question })
 
   const response = await openai.createChatCompletion({
     tmpMessages,
     model: "gpt-3.5-turbo",
   });
 
-  messages.push({ role: "assistant", content: response.data.choices[0].messages.content })
-  console.log(messages.length)
+  // messages.push({ role: "assistant", content: response.data.choices[0].messages.content })
+  
   res.end(JSON.stringify(response))
 })
 )
@@ -46,6 +46,8 @@ var server = app.listen(3000, function () {
   var port = server.address().port
 
   console.log('listening at 3000')
+
+  console.log(process.env.OPEN_KEY)
 
 })
 
