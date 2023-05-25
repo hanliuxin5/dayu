29fa8b349a73f47a9240bd02913ed7c6e3c13987
lychee <lychee930224@gmail.com>
2023-05-25 15:34:16 +0800

    stream res

--- dayu.js
+++ dayu.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const ai = require('openai')
+const timeout = require('connect-timeout')
+
 
 const configuration = new ai.Configuration({
   organization: "org-5YPkUu9Ohawr7i0JPaWsVUj3",
@@ -8,6 +10,7 @@ const configuration = new ai.Configuration({
 const openai = new ai.OpenAIApi(configuration);
 
 var app = express();
+app.use(timeout('180s'))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/" + "4cdf8c53538df917b054412f2de295c6.txt");
@@ -28,13 +31,32 @@ app.get('/process_get', runAsyncWrapper(async (req, res) => {
   const response = await openai.createChatCompletion({
     messages: messages,
     model: "gpt-4",
+    stream: true,
+  }, { responseType: "stream" });
+
+  var finalContent = ""
+
+  response.data.on('data', data => {
+    const lines = data.toString().split('\n').filter(line => line.trim() !== '');
+    for (const line of lines) {
+      const message = line.replace(/^data: /, '');
+      if (message === '[DONE]') {
+        messages.push({ role: "assistant", content: finalContent })
+        res.end()
+        return;
+      }
+      try {
+        const parsed = JSON.parse(message);
+        if (parsed.choices[0].delta.content != undefined) {
+          finalContent += parsed.choices[0].delta.content
+          res.write(parsed.choices[0].delta.content)
+        }
+        console.log(parsed.choices[0].delta);
+      } catch (error) {
+        console.error('Could not JSON parse stream message', message, error);
+      }
+    }
   });
-
-  const content = response.data.choices[0].message.content
-  messages.push({ role: "assistant", content: content })
-
-  res.end(JSON.stringify(content))
-
 })
 )
 
@@ -47,6 +69,10 @@ var server = app.listen(3000, function () {
   console.log('listening at 3000')
 
 })
+
+server.timeout = 180000
+server.requestTimeout = 180000
+server.setTimeout(180000)
 
 function runAsyncWrapper(callback) {
   return function (req, res, next) {
